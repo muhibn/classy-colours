6aee4059f85df4c472c5b9f67c6362827f28c2ed
Muhibullah Noorzad <mohebullah2015@gamil.com>
2026-02-01 04:30:44 +1100

    checking the github  connection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <div className="hero-section">
       <div className="hero-content">
-        <h1>Premium Painting Services</h1>
+        <h1>Github and firebase is connected </h1>
         <p>Transforming spaces with quality and creativity</p>
       </div>
     </div>
